753f29ab54dc60889dcb7768100e00e730c6b93e
Sahinogullari <mehmetalisahinogullari@hotmail.com>
2019-07-12 08:00:24 +0300

    Logging Handler

--- src/main/java/com/example/subscriber/base/LoggingHandler.java
+++ src/main/java/com/example/subscriber/base/LoggingHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 
 /**
@@ -22,64 +23,69 @@ import java.time.LocalDateTime;
 @Component
 public class LoggingHandler extends AbstractBaseComponent {
 
-  @Pointcut("within(com.example.subscriber.controller.*)")
-  public void controller() {
-  }
-
-  @Pointcut("execution(* com.example.subscriber.endpoint..*(..))")
-  public void endpoint() {
-
-  }
-
-  @Pointcut("execution(* *.*(..))")
-  protected void allMethod() {
-  }
-
-  @Before("execution(* com.example.subscriber.controller..*(..)) || execution(* com.example.subscriber.endpoint..*(..))")
-  public void logBefore(JoinPoint joinPoint) {
-
-    final StringBuilder builder = new StringBuilder();
-    builder.append(dateTimeFormatter.format(LocalDateTime.now()));
-    builder.append(" ");
-    final HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-    builder.append(request.getRequestURI());
-    builder.append("[");
-    builder.append(request.getMethod());
-    builder.append("]");
-    builder.append(getBody(joinPoint));
-    logger.info(builder.toString());
-  }
-
-  private String getBody(JoinPoint joinPoint) {
-    final StringBuilder builder = new StringBuilder();
-    final Object object = joinPoint.getArgs()[0];
-    try {
-      final Class<?> thisClass = Class.forName(object.getClass().getName());
-      builder.append(" [ ");
-      for (Field field : thisClass.getDeclaredFields()) {
-        field.setAccessible(true);
-        builder.append(field.getName())
-            .append(" = ")
-            .append(field.get(object))
-            .append(", ");
-      }
-      builder.append("]");
-    } catch (Exception e) {
-      logger.error(e.getMessage());
+    @Pointcut("within(com.example.subscriber.controller.*)")
+    public void controller() {
     }
-    return builder.toString();
-  }
 
-  @AfterThrowing(pointcut = "execution(* com.example.subscriber.controller..*(..)) || execution(* com.example.subscriber.endpoint..*(..))",
-      throwing = "exception")
-  public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
-    final StringBuilder builder = new StringBuilder();
-    builder.append(dateTimeFormatter.format(LocalDateTime.now()));
-    builder.append("An exception has been thrown in ");
-    builder.append(joinPoint.getSignature().getName()).append("()");
-    builder.append("Error Message: ");
-    builder.append(exception.getMessage());
-    logger.error(builder.toString());
-  }
+    @Pointcut("execution(* com.example.subscriber.endpoint..*(..))")
+    public void endpoint() {
+
+    }
+
+    @Pointcut("execution(* *.*(..))")
+    protected void allMethod() {
+    }
+
+    @Before("execution(* com.example.subscriber.controller..*(..)) || execution(* com.example.subscriber.endpoint..*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+
+        final StringBuilder builder = new StringBuilder();
+        builder.append(dateTimeFormatter.format(LocalDateTime.now()));
+        builder.append(" ");
+        final HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        builder.append(request.getRequestURI());
+        builder.append("[");
+        builder.append(request.getMethod());
+        builder.append("]");
+        builder.append(getBody(joinPoint));
+        logger.info(builder.toString());
+    }
+
+    private String getBody(JoinPoint joinPoint) {
+        final StringBuilder builder = new StringBuilder();
+        final Optional<Object> requestBody = Optional.of(joinPoint.getArgs())
+                .filter(args -> args.length > 0)
+                .map(args -> args[0]);
+        try {
+            builder.append(" [ ");
+            if (requestBody.isPresent()) {
+                final Class<?> thisClass = Class.forName(requestBody.get().getClass().getName());
+                for (Field field : thisClass.getDeclaredFields()) {
+                    field.setAccessible(true);
+                    builder.append(field.getName())
+                            .append(" = ")
+                            .append(field.get(requestBody.get()))
+                            .append(", ");
+                }
+            }
+            builder.append("]");
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return builder.toString();
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.example.subscriber.controller..*(..)) || execution(* com.example.subscriber.endpoint..*(..))",
+            throwing = "exception")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(dateTimeFormatter.format(LocalDateTime.now()));
+        builder.append("An exception has been thrown in ");
+        builder.append(joinPoint.getSignature().getName()).append("()");
+        builder.append("Error Message: ");
+        builder.append(exception.getMessage());
+        logger.error(builder.toString());
+        exception.printStackTrace();
+    }
 
 }
